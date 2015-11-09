ff5963d2b0cf7a271c16e0b650a14d841271901c
Florian Lepretre <florian.lprt@gmail.com>
2015-11-09 16:21:33 +0100

    Refactoring histoire 1

--- src/Engine.js
+++ src/Engine.js
@@ -1,28 +1,24 @@
-'use strict';
-
 var Engine = function () {
+    'use strict';
 
     // private attributes and methods
-    var board;
-    var size;
-
-    var initBoard = function () {
-        board = new Array(size);
-        for (var i = 0; i < size; ++i){
-            board[i] = new Array(size);
-        }
-
-        board[0][0] = board[2][4] = board[3][1] = board[4][3] = board[5][2] = board[5][5] = 'black';
-        board[0][1] = board[1][2] = board[3][3] = board[4][1] = board[4][5] = board[5][4] = 'green';
-        board[1][0] = board[1][4] = board[2][1] = board[4][2] = board[4][4] = board[5][0] = 'yellow';
-        board[0][2] = board[0][5] = board[1][1] = board[2][3] = board[3][5] = board[4][0] = 'white';
-        board[0][4] = board[1][3] = board[2][5] = board[3][0] = board[3][2] = board[5][3] = 'red';
-        board[0][3] = board[1][5] = board[2][0] = board[2][2] = board[3][4] = board[5][1] = 'blue';
-    };
+    var board,
+        size,
+        colors;
 
     var init = function () {
         size = 6;
-        initBoard();
+
+        colors = {bla: 0, gre: 1, whi: 2, blu: 3, red: 4, yel: 5};
+
+        board = [
+            [colors.bla, colors.gre, colors.whi, colors.blu, colors.red, colors.whi],
+            [colors.yel, colors.whi, colors.gre, colors.red, colors.yel, colors.blu],
+            [colors.blu, colors.yel, colors.blu, colors.whi, colors.bla, colors.red],
+            [colors.red, colors.bla, colors.red, colors.gre, colors.blu, colors.whi],
+            [colors.whi, colors.gre, colors.yel, colors.bla, colors.yel, colors.gre],
+            [colors.yel, colors.blu, colors.bla, colors.red, colors.gre, colors.bla]
+        ];
     };
 
     // public methods
