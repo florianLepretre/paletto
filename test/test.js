cf51a72ba05e50c54d7cf95454458a9c8af44d89
Florian Lepretre <florian.lprt@gmail.com>
2015-11-09 15:43:33 +0100

    Test histoire 1

--- test/test.js
+++ test/test.js
@@ -3,4 +3,14 @@
 var PalettoTestCase = TestCase("PalettoTestCase");
 
 PalettoTestCase.prototype.testStory1 = function () {
+    var engine = new Engine();
+
+    var size = engine.getSize();
+
+    for (var i = 0; i < size - 1; ++i) {
+        for (var j = 0; j < size - 1; ++j) {
+            assertNotEquals(engine.getCase(i, j), engine.getCase(i+1, j));
+            assertNotEquals(engine.getCase(i, j), engine.getCase(i, j+1));
+        }
+    }
 };
