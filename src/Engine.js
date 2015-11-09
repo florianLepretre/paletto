25142658ac34440fc837ff546ea8e723d418deba
Florian Lepretre <florian.lprt@gmail.com>
2015-11-09 15:58:53 +0100

    Implémentation histoire 1

--- src/Engine.js
+++ src/Engine.js
@@ -2,7 +2,37 @@
 
 var Engine = function () {
 
-// private attributes and methods
+    // private attributes and methods
+    var board;
+    var size;
 
-// public methods
+    var initBoard = function () {
+        board = new Array(size);
+        for (var i = 0; i < size; ++i){
+            board[i] = new Array(size);
+        }
+
+        board[0][0] = board[2][4] = board[3][1] = board[4][3] = board[5][2] = board[5][5] = 'black';
+        board[0][1] = board[1][2] = board[3][3] = board[4][1] = board[4][5] = board[5][4] = 'green';
+        board[1][0] = board[1][4] = board[2][1] = board[4][2] = board[4][4] = board[5][0] = 'yellow';
+        board[0][2] = board[0][5] = board[1][1] = board[2][3] = board[3][5] = board[4][0] = 'white';
+        board[0][4] = board[1][3] = board[2][5] = board[3][0] = board[3][2] = board[5][3] = 'red';
+        board[0][3] = board[1][5] = board[2][0] = board[2][2] = board[3][4] = board[5][1] = 'blue';
+    };
+
+    var init = function () {
+        size = 6;
+        initBoard();
+    };
+
+    // public methods
+    this.getSize = function () {
+        return size;
+    };
+
+    this.getCase = function (line, column) {
+        return board[line][column];
+    };
+
+    init();
 };
