a6ff8e5f3f7740fe8e001d8c02a643b39047df7f
Florian Lepretre <florian.lprt@gmail.com>
2015-11-26 16:04:46 +0100

    Correction d'un test

--- test/test64.js
+++ test/test64.js
@@ -8,20 +8,10 @@ PalettoTestCase64.prototype.testStory8 = function () {
     var size = engine.getSize();
     assertTrue(size === 8);
 
-    /*for (var i = 0; i < size - 1; ++i) {
+    for (var i = 0; i < size - 1; ++i) {
         for (var j = 0; j < size - 1; ++j) {
             assertNotEquals(engine.getCase(i, j), engine.getCase(i+1, j));
             assertNotEquals(engine.getCase(i, j), engine.getCase(i, j+1));
         }
-    }*/
-
-    var line;
-
-    for (var k = 0; k < size; ++k) {
-        line = '';
-        for (var l = 0; l < size; ++l) {
-            line += engine.getCase(k,l) + ' ';
-        }
-        console.log(line);
     }
 };
